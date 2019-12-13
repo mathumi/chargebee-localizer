b492d48754486b46ebca7f33cc6f62cd2f7198be
cb-mathu <mathumitha@chargebee.com>
2019-12-13 19:04:19 +0530

    table

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,44 +1,44 @@
 <template>
   <div id="app">
-   <b-navbar>
-        <template slot="brand">
-            <b-navbar-item class="logo" tag="router-link" :to="{ path: '/' }">
-                <img class="logo__img" src="@/assets/images/logo.png" >Localizer
-            </b-navbar-item>
-        </template>
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item class="logo" tag="router-link" :to="{ path: '/' }">
+          <img class="logo__img" src="@/assets/images/logo.png" />Localizer
+        </b-navbar-item>
+      </template>
 
-        <template slot="end">
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Create Collection</strong>
-                    </a>
-                    <!-- <a class="button is-light">
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Create Collection</strong>
+            </a>
+            <!-- <a class="button is-light">
                         
                     </a> -->
-                </div>
-            </b-navbar-item>
-        </template>
+          </div>
+        </b-navbar-item>
+      </template>
     </b-navbar>
-      <transition name="fade" mode="out-in">
-              <router-view />
-            </transition>
+    <transition name="fade" mode="out-in">
+      <div class="wrapper">
+      <router-view />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
+import { Component, Vue } from "vue-property-decorator";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 Vue.use(Buefy);
 
 @Component({
-  components: {
-  },
+  components: {}
 })
 export default class App extends Vue {}
 </script>
 
-<style lang="scss" src="@/assets/styles/main.scss">
-</style>
+<style lang="scss" src="@/assets/styles/main.scss"></style>
