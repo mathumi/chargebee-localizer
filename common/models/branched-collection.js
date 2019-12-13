95599963677b668ff14c3e955f8cda4f95ad21cd
Dinesh <dinesh@chargebee.com>
2019-12-14 01:53:07 +0530

    Publish branch remote method

--- common/models/branched-collection.js
+++ common/models/branched-collection.js
@@ -1,6 +1,24 @@
 'use strict';
 
 module.exports = function(Branchedcollection) {
+  Branchedcollection.publish = function(collectionId, cb) {
+    Branchedcollection.findOne({
+      id: collectionId
+    }, function(err, collection) {
+      if(err || !collection) return cb(err)
+      collection.updateAttributes({
+        publised_texts_version: collection.draft_texts_version,
+        draft_texts_version: ''
+      }, cb)
+    })
+  }
+
+  Branchedcollection.remoteMethod('publish',{
+    accepts: {arg: 'collectionId', type: 'number'},
+    returns: {arg: 'collection', type: 'Branchedcollection', "root": true},
+    http: {path: '/:collectionId/publish', verb: 'post', errorStatus: 400}
+  });
+  
   Branchedcollection.observe('before save', async function(ctx) {
     ctx.instance.updated_at = new Date()
     return;
