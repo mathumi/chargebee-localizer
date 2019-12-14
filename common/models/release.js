531f0975e2d2f07576b7f4aeb47f08c47fdc78d8
Dinesh <dinesh@chargebee.com>
2019-12-15 04:17:45 +0530

    Get collections by release API

--- common/models/release.js
+++ common/models/release.js
@@ -1,7 +1,7 @@
 'use strict';
 const async = require('async')
 
-module.exports = function(Release) {
+module.exports = function (Release) {
   Release.disableRemoteMethod("create", true);
   Release.remoteMethod('createRelease', {
     description: 'Create a release from a branch',
@@ -11,58 +11,58 @@ module.exports = function(Release) {
         description: 'name, description and branchId'
       },
     ],
-    returns: {arg: 'release', type: 'object', "root": true},
-    http: {path: '/', verb: 'post', errorStatus: 400}
+    returns: { arg: 'release', type: 'object', "root": true },
+    http: { path: '/', verb: 'post', errorStatus: 400 }
   });
 
   Release.remoteMethod('getReleases', {
-    returns: {arg: 'releases', type:'array', root: true},
-    http: {verb: 'get', path: '/', errorStatus: 400}
+    returns: { arg: 'releases', type: 'array', root: true },
+    http: { verb: 'get', path: '/', errorStatus: 400 }
   })
 
   Release.remoteMethod('getReleaseText', {
     accepts: [{
-        arg: 'locale', required: true, type: 'string', http: {source: 'query'}
-      }, {
-        arg: 'release', type: 'string', http: {source: 'query'}
-      }, {
-        arg: 'collections', type: 'string', http: {source: 'query'}
-      }],
-      returns: {arg: 'texts', type: 'object', root: true},
-      http: {verb: 'get', path: '/release/texts', errorStatus: 400}
+      arg: 'locale', required: true, type: 'string', http: { source: 'query' }
+    }, {
+      arg: 'release', type: 'string', http: { source: 'query' }
+    }, {
+      arg: 'collections', type: 'string', http: { source: 'query' }
+    }],
+    returns: { arg: 'texts', type: 'object', root: true },
+    http: { verb: 'get', path: '/release/texts', errorStatus: 400 }
   })
 
-  
-  Release.getReleases = function(cb) {
-    Release.find(function(err, releases) {
-      if(err) return cb(err);
+
+  Release.getReleases = function (cb) {
+    Release.find(function (err, releases) {
+      if (err) return cb(err);
       cb(null, releases);
     })
   }
-  Release.createRelease = function(data, cb) {
+  Release.createRelease = function (data, cb) {
     const { name, description, branchId } = data;
-    if(!(name && description && branchId)) {
+    if (!(name && description && branchId)) {
       return new Error('Missing parameters')
     }
     const Branch = Release.app.models.Branches
 
     async.waterfall([
       // Create a release
-      function(callback) {
+      function (callback) {
         Release.create({
           name,
           description
-        }, function(err, release) {
+        }, function (err, release) {
           return callback(err, release)
         })
       },
       // Get current branch with its collections and its text
-      function(release, callback) {
-        const filters = { 
-          include: { 
+      function (release, callback) {
+        const filters = {
+          include: {
             relation: 'collections',
             scope: {
-              fields: [ 'id', 'name', 'description', 'publised_texts_version' ],
+              fields: ['id', 'name', 'description', 'publised_texts_version'],
               include: {
                 relation: 'text',
                 scope: {
@@ -75,15 +75,68 @@ module.exports = function(Release) {
             }
           }
         };
-        Branch.findById(branchId, filters, function(err, branch) {
+        Branch.findById(branchId, filters, function (err, branch) {
           return callback(err, branch, release)
         })
       }
     ], cb);
-    
+
   }
 
-  Release.observe('before save', async function(ctx) {
+  Release.remoteMethod('getCollections', {
+    accepts: [
+      {
+        arg: 'releaseId', required: true, type: 'string'
+      }
+    ],
+    returns: { arg: 'collections', type: 'object', root: true },
+    http: { verb: 'get', path: '/:releaseId/collections', errorStatus: 400 }
+  })
+
+  Release.getCollections = function(releaseId, callback) {
+    const Collection = Release.app.models.ReleaseCollection
+    Release.findById(releaseId)
+      .then(release => {
+        if(!release) throw new Error('Not found')
+
+        return Collection.find({
+          where: {release_id: releaseId},
+          include: {
+            relation: 'text',
+            scope: {
+              fields: ['key']
+            }
+          }
+        })
+      })
+      .then(data => {
+        const rawData = serialize(data, Collection)
+        const collectionMap = {}
+        rawData.data.map(collection => {
+          collectionMap[collection.id] = {
+            id: collection.id,
+            handle: collection.attributes.handle,
+            name: collection.attributes.name,
+            description: collection.attributes.description,
+            keyCount: 0
+          }
+        })
+
+        if(rawData.included) {
+          rawData.included.map(key => {
+            const _collection = collectionMap[key.attributes.collection_id]
+            _collection.keyCount = _collection.keyCount + 1
+          })
+        }
+
+        const formattedData = Object.values(collectionMap)
+        
+        cb(null, formattedData)
+      })
+      .catch(err => cb(err))
+  }
+
+  Release.observe('before save', async function (ctx) {
     ctx.instance.updated_at = new Date()
     return;
   });
