157547735039f93bb5925581f6e6f399af5b38dc
cb-bharathvaj <bharathvaj@chargebee.com>
2019-12-14 08:22:20 +0530

    wip: new branch

---
+++ ui/src/components/modals/NewBranch.vue
@@ -0,0 +1,40 @@
+<template>
+  <form action>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">New Branch {{hello}}</p>
+      </header>
+      <section class="modal-card-body">
+        <b-field label="Branch Name">
+          <b-input type="text" placeholder="feature/snap" required></b-input>
+        </b-field>
+        <b-field label="Fork From">
+          <b-select :value="selectedResource" icon="source-branch">
+            <option
+              v-for="filter in resourceFilters"
+              :value="filter.id"
+              :key="filter.id"
+            >{{ filter.name }}</option>
+          </b-select>
+        </b-field>
+
+        <b-field label="Description">
+          <b-input type="textarea" placeholder="Super snappingly scalable"></b-input>
+        </b-field>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-primary">Create</button>
+      </footer>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewBranch",
+  props: ["selectedResource", "resourceFilters"]
+};
+</script>
+
+<style>
+</style>
--- ui/src/views/BranchPage.vue
+++ ui/src/views/BranchPage.vue
@@ -2,13 +2,15 @@
   <div>
     <section>
       <div class="block">
-        <b-select :value="selectedResource" @input="updateSelectedResource">
+        <b-select :value="selectedResource" @input="updateSelectedResource" icon="source-branch">
           <option
             v-for="filter in resourceFilters"
             :value="filter.id"
             :key="filter.id"
           >{{ filter.name }}</option>
         </b-select>
+
+        <b-button type="is-primary" icon-left="source-branch" @click="openNewBranchModal">New Branch</b-button>
       </div>
       <b-tabs v-model="activeTab">
         <b-tab-item label="Collections" icon="package">
@@ -22,6 +24,9 @@
         </b-tab-item>
       </b-tabs>
     </section>
+    <b-modal :active.sync="isNewBranchModalActive" :width="640">
+      <NewBranch :resourceFilters="resourceFilters" :selectedResource="selectedResource" />
+    </b-modal>
   </div>
 </template>
 
@@ -29,18 +34,21 @@
 import Collections from "@/components/branch/tabs/Collections";
 import Branches from "@/components/branch/tabs/Branches";
 import Releases from "@/components/branch/tabs/Releases";
+import NewBranch from "@/components/modals/NewBranch.vue";
 
 export default {
   components: {
     Collections,
     Branches,
-    Releases
+    Releases,
+    NewBranch
   },
   data() {
     return {
       activeTab: 0,
       showTabs: false,
       selectedResource: "master",
+      isNewBranchModalActive: false,
       resourceFilters: [
         {
           name: "master",
@@ -78,10 +86,18 @@ export default {
       if (newValue) {
         this.$router.replace(`/tree/${newValue}`);
       }
+    },
+    openNewBranchModal() {
+      this.isNewBranchModalActive = true;
     }
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.block {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
