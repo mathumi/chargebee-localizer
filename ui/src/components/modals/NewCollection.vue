dae3e849ef931f1ba65e5c5878182e3bdac5e2f0
cb-bharathvaj <bharathvaj@chargebee.com>
2019-12-15 12:05:30 +0530

    feat: publish branch

--- ui/src/components/modals/NewCollection.vue
+++ ui/src/components/modals/NewCollection.vue
@@ -1,37 +1,32 @@
 <template>
-  <form action>
-    <div class="modal-card">
-      <header class="modal-card-head">
-        <p class="modal-card-title">New Collection</p>
-      </header>
-      <section class="modal-card-body">
-        <b-field label="Collection Name">
-          <b-input type="text" placeholder="Snap" required></b-input>
-        </b-field>
-        <b-field label="Description">
-          <b-input type="textarea" placeholder="Super snappingly scalable"></b-input>
-        </b-field>
-        <b-field label="Collections">
-          <b-upload drag-drop>
-            <section class="section">
-                <div class="content has-text-centered">
-                    <p>
-                        <b-icon
-                            icon="upload"
-                            size="is-large">
-                        </b-icon>
-                    </p>
-                    <p>Drop your translation file here</p>
-                </div>
-            </section>
-          </b-upload>
-        </b-field>
-      </section>
-      <footer class="modal-card-foot">
-        <button class="button is-primary">Create</button>
-      </footer>
-    </div>
-  </form>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">New Collection</p>
+    </header>
+    <section class="modal-card-body">
+      <b-field label="Collection Name">
+        <b-input type="text" placeholder="Snap" required></b-input>
+      </b-field>
+      <b-field label="Description">
+        <b-input type="textarea" placeholder="Super snappingly scalable"></b-input>
+      </b-field>
+      <b-field label="Collections">
+        <b-upload drag-drop>
+          <section class="section">
+            <div class="content has-text-centered">
+              <p>
+                <b-icon icon="upload" size="is-large"></b-icon>
+              </p>
+              <p>Drop your translation file here</p>
+            </div>
+          </section>
+        </b-upload>
+      </b-field>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-primary">Create</button>
+    </footer>
+  </div>
 </template>
 
 <script>
--- ui/src/components/modals/NewRelease.vue
+++ ui/src/components/modals/NewRelease.vue
@@ -1,25 +1,23 @@
 <template>
-  <form action>
-    <div class="modal-card">
-      <header class="modal-card-head">
-        <p class="modal-card-title">Publish Branch</p>
-      </header>
-      <section class="modal-card-body">
-        <b-field label="Release Name">
-          <b-input type="text" v-model="releaseName" placeholder="Winter Release" required></b-input>
-        </b-field>
-        <b-field label="Release Id">
-          <b-input type="text" :value="releaseId" placeholder="winter_release" readonly></b-input>
-        </b-field>
-        <b-field label="Description">
-          <b-input type="textarea" placeholder="Fall Edition"></b-input>
-        </b-field>
-      </section>
-      <footer class="modal-card-foot">
-        <button class="button is-primary">Publish</button>
-      </footer>
-    </div>
-  </form>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Publish Branch</p>
+    </header>
+    <section class="modal-card-body">
+      <b-field label="Release Name">
+        <b-input type="text" v-model="releaseName" placeholder="Winter Release" required></b-input>
+      </b-field>
+      <b-field label="Release Id">
+        <b-input type="text" :value="releaseId" placeholder="winter_release" readonly></b-input>
+      </b-field>
+      <b-field label="Description">
+        <b-input type="textarea" v-model="description" placeholder="Fall Edition"></b-input>
+      </b-field>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-primary" :loading="loading" @click="createRelease">Publish</button>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -39,6 +37,29 @@ export default {
         .replace(/[^a-zA-Z ]/g, "")
         .toLowerCase()
         .replace(/ /g, "_");
+    }
+  },
+  methods: {
+    createRelease() {
+      this.loading = true;
+      const newRelease = {
+        name: this.releaseId,
+        description: this.description
+      };
+
+      this.$store
+        .dispatch("publishDraftBranch", {
+          id: this.branchId,
+          payload: newRelease
+        })
+        .then(() => {
+          this.$success("Branch published");
+          this.$parent.close();
+        })
+        .catch(this.$error)
+        .finally(() => {
+          this.loading = false;
+        });
     }
   }
 };
