a3fd252896cc2eceadd322bd8e215d3a70a15b6a
cb-mathu <mathumitha@chargebee.com>
2019-12-16 13:33:45 +0530

    deployment tags

--- ui/src/views/DeploymentsPage.vue
+++ ui/src/views/DeploymentsPage.vue
@@ -1,22 +1,36 @@
 <template>
   <div>
-    <div class="flex"> 
-    <div class="is-size-3 mar--b-sm flex-grow">Deployments</div>
-        <b-button type="is-primary deploy-button" icon-left="plus" class="mar--l-mi" @click="openDeployModal"
-      >Create Deployment</b-button
-    ></div>
+    <div class="flex">
+      <div class="is-size-3 mar--b-sm flex-grow">Deployments</div>
+      <b-button
+        type="is-primary deploy-button"
+        icon-left="plus"
+        class="mar--l-mi"
+        @click="openDeployModal"
+        >Create Deployment</b-button
+      >
+    </div>
     <div class="deploy-item" v-for="data in deploySchema" :key="data.value">
-     <h3>{{data.displayName}}</h3>
-     <div><b>Version:</b> {{ data.value }}</div>
-      <div><b>Priority:</b> {{ data.priority }}</div>
+      <h3>{{ data.displayName }}</h3>
       <div>{{ data.comment }}</div>
-            <b-table :data="data.rules">
-        <template slot-scope="props">
-          <b-table-column label="Attribute">{{ props.row.attribute }}</b-table-column>
-          <b-table-column label="Operator">{{ props.row.operator }}</b-table-column>
-          <b-table-column label="Value">{{ props.row.value}}</b-table-column>
-        </template>
-      </b-table>
+
+      <div class="flex mar--t-st">
+        <div class="flex-grow">
+          <div
+            v-for="(rule, index) in data.rules"
+            class="mar--b-xs deploy-tags"
+          >
+            <span class="tag is-light tag-attribute ">{{ rule.attribute }}</span>
+            <span class="tag is-light tag-operator">{{ rule.operator }}</span>
+            <span class="tag is-light tag-value">{{ rule.value }}</span>
+            <span v-if="index !== data.rules.length - 1"><b>AND</b></span>
+          </div>
+        </div>
+        <div class="flex-grow">
+          <div><b>Version:</b> {{ data.value }}</div>
+          <div><b>Priority:</b> {{ data.priority }}</div>
+        </div>
+      </div>
     </div>
     <b-modal :active.sync="isNewDeployModalActive" :width="640">
       <deploy-modal />
@@ -32,7 +46,7 @@ export default {
     return {
       deploySchema: [
         {
-          displayName: 'deploy',
+          displayName: "deploy",
           name: "app.copy.version",
           value: "copy-1.0.0",
           priority: 2,
@@ -59,7 +73,8 @@ export default {
           name: "app.copy.version",
           value: "copy-2.0.0",
           priority: 2,
-          comment: "hello hellohellohellohellohello hellohello hellohello hello",
+          comment:
+            "hello hellohellohellohellohello hellohello hellohello hello",
           rules: [
             {
               attribute: "environment",
@@ -94,22 +109,40 @@ export default {
 </script>
 
 <style lang="scss">
-.deploy-item{
+.deploy-item {
   padding-bottom: 30px;
-    margin-top: 15px;
-    padding-left: 0;
-    padding-right: 0;
-    &:not(:last-child) {
-      border-bottom: 2px solid #dbdbdb;
-    }
-    table {
-      border: 1px solid #dbdbdb !important;
+  margin-top: 30px;
+  padding-left: 0;
+  padding-right: 0;
+  &:not(:last-child) {
+    border-bottom: 2px solid #dbdbdb;
+  }
+  table {
+    border: 1px solid #dbdbdb !important;
     padding: 4px 15px;
     margin-top: 12px;
-    }
+  }
 }
-.deploy-button{
-      min-width: 110px;
-    height: 36px;
+.deploy-button {
+  min-width: 110px;
+  height: 36px;
+}
+.deploy-tags {
+  .tag {
+    background-color: #f9f6f6 !important;
+    font-size: 14px;
+    margin-right: 12px;
+    &-{
+      &attribute {
+        color: #336911 !important;
+      }
+       &operator {
+        color: #0c1369 !important;
+      }
+       &value {
+        color: #3367d6 !important;
+      }
+    }
+  }
 }
 </style>
